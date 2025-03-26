b48f74ccbebcb979943a8889f8eadfe4944938e4
Abdelrahman <bsmattok@gmail.com>
2025-03-26 17:05:02 +0200

    Added Update Product API

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
 
 app.get('/api/products', async (req, res) => {
     try{
@@ -34,6 +36,23 @@ app.post('/api/products', async (req, res) => {
         res.status(201).json({success: true, data: newProduct});
     } catch{
         res.status(500).json({success: false, message: error.message});
+    }
+});
+
+
+app.put('/api/products/:id', async (req, res) => {
+    const { id } = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).send('No product with that id');
+    }
+
+    const product = req.body;
+
+    try{
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, product, {new: true});
+        res.status(200).json({success: true, data: updatedProduct});
+    } catch(error){
+        res.status(500).json({success: false, message: "Server Error"});
     }
 });
 
