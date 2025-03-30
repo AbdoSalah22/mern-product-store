7c7347b32388cc10b1da9c1712cc453991a26c70
Abdelrahman <bsmattok@gmail.com>
2025-03-30 15:19:56 +0200

    Added Delete functionality

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,19 +1,41 @@
-import { Box, Heading, Image, Text } from '@chakra-ui/react'
+import { Box, Button, Heading, HStack, IconButton, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import { toaster } from "../components/ui/toaster"
 import { useColorModeValue } from './ui/color-mode'
+import { MdEdit, MdDelete } from "react-icons/md";
+import { useProductStore } from '../store/product';
+
 
 const ProductCard = ({ product }) => {
     const textColor = useColorModeValue("gray.800", "whiteAlpha.900");
     const bg = useColorModeValue("white", "gray.800");
+
+    const { deleteProduct } = useProductStore();
+    const handleDeleteProduct = async (pid) => {
+        const { success, message } = await deleteProduct(pid);
+        console.log(success, message);
+        if (!success) {
+            toaster.create({
+                description: "error!!",
+                type: "error",
+            })
+        }
+        else {
+            toaster.create({
+                description: "success!!",
+                type: "success",
+            })
+        }
+    }
     return (
         <Box
             m={2}
             shadow={"lg"}
             rounded={"lg"}
             overflow={"hidden"}
-            transition={"all 0.2s"}
+            transition={"all 0.3s"}
             _hover={{
-                transform: "scale(1.05)",
+                transform: "scale(1.025)",
                 shadow: "xl",
             }}
             bg={bg}>
@@ -26,6 +48,15 @@ const ProductCard = ({ product }) => {
                 <Text fontWeight={'bold'} fontSize={'xl'} color={textColor} mb={4}>
                     ${product.price}
                 </Text>
+
+                <HStack spacing={2}>
+                    <Button colorScheme={"blue"}>
+                        <MdEdit />
+                    </Button>
+                    <Button onClick={() => handleDeleteProduct(product._id)} colorScheme={"red"}>
+                        <MdDelete />
+                    </Button>
+                </HStack>
             </Box>
         </Box>
     )
